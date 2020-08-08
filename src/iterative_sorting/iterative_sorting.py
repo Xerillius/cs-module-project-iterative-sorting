e6caeaebd650cd240292542dfd009efdff925757
Christopher Quinn <caquinn.86@gmail.com>
2020-08-07 19:54:42 -0700

    finished assignment

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,24 +1,23 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    for i in range(0, len(arr)):
         cur_index = i
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
-
-
-        # TO-DO: swap
-        # Your code here
+        for x in range(cur_index + 1, len(arr)):
+            if arr[smallest_index] > arr[x]:
+                smallest_index = x
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
-
+    for i in range(len(arr)):
+        for x in range(0, len(arr)-i-1):
+            if arr[x] > arr[x+1]:
+                arr[x], arr[x+1] = arr[x+1], arr[x]
 
     return arr
 
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,23 @@
+import math
+
 def linear_search(arr, target):
-    # Your code here
-
-
+    for i, v in enumerate(arr):
+        if v == target:
+            return i
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
-
+    start = 0
+    end = arr.__len__() - 1
+    while start <= end:
+        mid = math.ceil((start + (end - 1)) / 2)
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
 
     return -1  # not found
